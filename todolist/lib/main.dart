55c81fb5a9dff59cf4eeb7615625f2a573cfb7fd
Master-Bibash <dokfreak142@gmail.com>
2023-11-27 12:38:29 +0545

    project initialised

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -1,1 +1,27 @@
-print("hello");
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Text("Lets make a todo app "),
+          ],
+        ),
+      ),
+    );
+  }
+}
