9c5191b4124f87021525475d69969d64e97b6888
Saroj Ghalan <jmestmnag@gmail.com>
2023-11-27 12:30:20 +0545

    Initilizing

---
+++ todolist/lib/screens/todo_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List') ,
+      ),
+    );
+  }
+}
